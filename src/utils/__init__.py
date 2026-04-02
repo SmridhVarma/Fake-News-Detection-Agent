df63e44889a2e28cf8c6b05f572069917c55d10a
SmridhVarma <smridhv@gmail.com>
2026-04-03 01:22:21 +0800

    added datasets

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,13 @@
 from src.utils.preprocessing import clean_text
 from src.utils.model_io import load_model, save_model
 from src.utils.prompts import FACT_CHECK_PROMPT
+from src.utils.fetch_article import fetch_article_content, is_url
 
 __all__ = [
     "clean_text",
     "load_model",
     "save_model",
     "FACT_CHECK_PROMPT",
+    "fetch_article_content",
+    "is_url",
 ]
